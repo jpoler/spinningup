8d4fb57f4288ada5d72dd724d842c0606f1e7358
Jon Poler <jonathan.poler@gmail.com>
2023-01-23 15:46:15 -0500

    ddpg: add docstring

--- spinup/algos/mytorch/ddpg/ddpg.py
+++ spinup/algos/mytorch/ddpg/ddpg.py
@@ -11,6 +11,7 @@ from spinup.algos.mytorch.base.algorithm import Algorithm
 from spinup.algos.mytorch.base.buffer import ReplayBuffer
 from spinup.utils.logx import EpochLogger
 
+# TODO move to util module
 def print_computation_graph(root, indent=0):
     if not root:
         return
@@ -204,6 +205,93 @@ def ddpg(
         save_freq=10,
         use_gpu=False,
 ):
+    """
+    Deep Deterministic Policy Gradient (DDPG)
+
+
+    Args:
+        env_fn : A function which creates a copy of the environment.
+            The environment must satisfy the OpenAI Gym API.
+
+        actor_critic: The constructor method for a PyTorch Module with an ``act``
+            method, a ``pi`` module, and a ``q`` module. The ``act`` method and
+            ``pi`` module should accept batches of observations as inputs,
+            and ``q`` should accept a batch of observations and a batch of
+            actions as inputs. When called, these should return:
+
+            ===========  ================  ======================================
+            Call         Output Shape      Description
+            ===========  ================  ======================================
+            ``act``      (batch, act_dim)  | Numpy array of actions for each
+                                           | observation.
+            ``pi``       (batch, act_dim)  | Tensor containing actions from policy
+                                           | given observations.
+            ``q``        (batch,)          | Tensor containing the current estimate
+                                           | of Q* for the provided observations
+                                           | and actions. (Critical: make sure to
+                                           | flatten this!)
+            ===========  ================  ======================================
+
+        ac_kwargs (dict): Any kwargs appropriate for the ActorCritic object
+            you provided to DDPG.
+
+        seed (int): Seed for random number generators.
+
+        steps_per_epoch (int): Number of steps of interaction (state-action pairs)
+            for the agent and the environment in each epoch.
+
+        epochs (int): Number of epochs to run and train agent.
+
+        replay_size (int): Maximum length of replay buffer.
+
+        gamma (float): Discount factor. (Always between 0 and 1.)
+
+        polyak (float): Interpolation factor in polyak averaging for target
+            networks. Target networks are updated towards main networks
+            according to:
+
+            .. math:: \\theta_{\\text{targ}} \\leftarrow
+                \\rho \\theta_{\\text{targ}} + (1-\\rho) \\theta
+
+            where :math:`\\rho` is polyak. (Always between 0 and 1, usually
+            close to 1.)
+
+        pi_lr (float): Learning rate for policy.
+
+        q_lr (float): Learning rate for Q-networks.
+
+        q_weight_decay(float): L2 regularization coefficient for critic.
+
+        batch_size (int): Minibatch size for SGD.
+
+        start_steps (int): Number of steps for uniform-random action selection,
+            before running real policy. Helps exploration.
+
+        update_after (int): Number of env interactions to collect before
+            starting to do gradient descent updates. Ensures replay buffer
+            is full enough for useful updates.
+
+        update_every (int): Number of env interactions that should elapse
+            between gradient descent updates. Note: Regardless of how long
+            you wait between updates, the ratio of env steps to gradient steps
+            is locked to 1.
+
+        act_noise (float): Stddev for Gaussian exploration noise added to
+            policy at training time. (At test time, no noise is added.)
+
+        num_test_episodes (int): Number of episodes to test the deterministic
+            policy at the end of each epoch.
+
+        max_ep_len (int): Maximum length of trajectory / episode / rollout.
+
+        logger_kwargs (dict): Keyword args for EpochLogger.
+
+        save_freq (int): How often (in terms of gap between epochs) to save
+            the current policy and value function.
+
+        use_gpu (bool): Which device to use for both acting and updating.
+
+    """
     ac_kwargs = ac_kwargs or {}
     logger_kwargs = logger_kwargs or {}
 
