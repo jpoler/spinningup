092d87bbf6af2ac956380659068d89f02c31e157
Jon Poler <jonathan.poler@gmail.com>
2023-03-12 15:03:38 -0400

    ppo benchmark script

---
+++ scripts/ppo_benchmarks.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for env in "HalfCheetah-v3" "Swimmer-v3" "Hopper-v3" "Walker2d-v3" "Ant-v3" "Humanoid-v3"; do
+    python -m spinup.run ppo_mytorch \
+           --seed 0 10 20 \
+           --steps_per_epoch 2048 \
+           --train_iters 10 \
+           --train_minibatches 32 \
+           --epochs 1465 \
+           --gamma 0.99 \
+           --lam 0.95 \
+           --clip_ratio 0.2 \
+           --pi_lr 0.0003 \
+           --vf_lr 0.001 \
+           --target_kl 0.01  \
+           --entropy_bonus_coef 0.0 \
+           --hid "[64,64]" \
+           --env "$env" \
+           --exp_name "ppo-benchmark-mytorch-$(echo $env | tr '[:upper:]' '[:lower:]')-$(date +%s)"
+done
